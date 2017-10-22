0b0eff5abdeb043ce6f13272ca8425cd74a1efa2
DESKTOP-MLO1IB3\Dmitry <dmitry.bilevich@gmail.com>
2017-10-22 17:59:23 +0300

    no message

---
+++ HomeWorkDay2/NUnitTest/NumberHandlerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using HomeWorkDay2;
+
+namespace NUnitTest
+{
+    [TestFixture]
+    public class NumberHandlerTest
+    {
+        private int ConvertBitArrayToNumber(int[] array)
+        {
+            var bitArray = new BitArray(32);
+            for (int i = 0; i < 32; i++)
+            {
+                bitArray[i] = array[i] == 1;
+            }
+
+            array = new int[1];
+            bitArray.CopyTo(array, 0);
+
+            return array[0];
+        }
+
+        [Test]
+        public void InsertNunTest()
+        {
+            NumberHandler handler = new NumberHandler();
+            var result = handler.InsertNun(15, 15, 0, 0);
+            int number = ConvertBitArrayToNumber(result);
+            Assert.AreEqual(number, 15);
+
+            result = handler.InsertNun(8, 15, 0, 0);
+            number = ConvertBitArrayToNumber(result);
+            Assert.AreEqual(number, 9);
+
+            result = handler.InsertNun(8, 15, 3, 8);
+            number = ConvertBitArrayToNumber(result);
+            Assert.AreEqual(number, 120);
+        }
+    }
+}
