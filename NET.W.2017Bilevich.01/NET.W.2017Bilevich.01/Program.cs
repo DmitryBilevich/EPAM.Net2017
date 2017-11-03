556f010095b6b94b75a90394ccf618402a4cc097
DESKTOP-MLO1IB3\Dmitry <dmitry.bilevich@gmail.com>
2017-11-03 10:29:36 +0300

    Home Task Day 01

---
+++ NET.W.2017Bilevich.01/NET.W.2017Bilevich.01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NET.W._2017Bilevich._01.Sort;
+
+namespace NET.W._2017Bilevich._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrayOne = { 9, 1, 5, 12, 3, 7, 9, 18, 5, 91, 6, 4, 27 };
+            SortArray.Quicksort (arrayOne);
+            foreach (int item in arrayOne)
+            {
+                System.Console.Write(item + "  ");
+            }
+            Console.WriteLine();
+
+            int[] arrayTwo = { 2, 7, 6, 4, 2, 6, 8, 79, 56, 23, 18, 30 };
+            arrayTwo=SortArray.MergeSort(arrayTwo);
+            foreach (int item in arrayTwo)
+            {
+                System.Console.Write(item  + "  ");
+            }
+        }
+    }
+}
---
+++ NET.W.2017Bilevich.01/NET.W.2017Bilevich.01/Sort/SortArray.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2017Bilevich._01.Sort
+{
+   public class SortArray
+    {
+        public static void Quicksort(int[] arr)
+        {
+            if (arr.Length > 1) Quicksort(arr, 0, arr.Length - 1);
+        }
+
+        private static void Quicksort(int[] array, int first, int last)
+        {
+            int pivot = array[(last - first) / 2 + first];
+            int temp;
+            int i = first, j = last;
+            while (i <= j)
+            {
+                while (array[i] < pivot && i <= last) ++i;
+
+                while (array[j] > pivot && j >= first) --j;
+
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    ++i; --j;
+                }
+            }
+            if (j > first) Quicksort(array, first, j);
+            if (i < last) Quicksort(array, i, last);
+        }
+
+        
+
+        public static int[] MergeSort(int[] buff)
+        {            
+            if (buff.Length > 1)
+            {
+                
+                int[] left = new int[buff.Length / 2];                
+                int[] right = new int[buff.Length - left.Length];
+               
+                for (int i = 0; i < left.Length; i++)
+                {
+                    left[i] = buff[i];
+                }
+                for (int i = 0; i < right.Length; i++)
+                {
+                    right[i] = buff[left.Length + i];
+                }
+                
+                if (left.Length > 1)
+                    left = MergeSort(left);
+                if (right.Length > 1)
+                    right = MergeSort(right);
+                
+                buff = MergeSort(left, right);
+            }
+              
+            return buff;
+        }
+
+        private static int[] MergeSort(int[] left, int[] right)
+        {
+            
+            int[] buff = new int[left.Length + right.Length];
+            
+            int i = 0;  
+            int l = 0;  
+            int r = 0;  
+            for (; i < buff.Length; i++)
+            {
+                
+                if (r >= right.Length)
+                {
+                    buff[i] = left[l];
+                    l++;
+                }
+                
+                else if (l < left.Length && left[l] < right[r])
+                {
+                    buff[i] = left[l];
+                    l++;
+                }
+                
+                else
+                {
+                    buff[i] = right[r];
+                    r++;
+                                        
+                }
+            }
+            
+            return buff;
+        }
+    }
+}
